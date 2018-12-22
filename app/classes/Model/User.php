4a5be7ddf5432e81aa9c5fb008f46ab23cfd0193
João Melo <35934859+joaopcm@users.noreply.github.com>
2018-12-22 00:26:10 -0200

    Adiciona CRUD de usuários

--- app/classes/Model/User.php
+++ app/classes/Model/User.php
@@ -57,6 +57,73 @@ class User extends Model {
         $_SESSION[User::SESSION] = NULL;
     }
 
+    /**
+     * Retorna todos os usuários cadastrados
+     */
+    public static function listAll()
+    {
+        $sql = new Sql();
+        return $sql->select('SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson');
+    } 
+
+    /**
+     * Cadastra um usuário no banco de dados
+     */
+    public function save()
+    {
+        $sql = new Sql();
+        $results = $sql->select('CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)', array(
+            ':desperson' => $this->getdesperson(),
+            ':deslogin' => $this->getdeslogin(),
+            ':despassword' => $this->getdespassword(),
+            ':desemail' => $this->getdesemail(),
+            ':nrphone' => $this->getnrphone(),
+            ':inadmin' => $this->getinadmin()
+        ));
+        $this->setData($results[0]);
+    }
+
+    /**
+     * Retorna os dados de um usuário pelo ID
+     */
+    public function get($id)
+    {
+        $sql = new Sql();
+        $results = $sql->select('SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser', array(
+            ':iduser' => $id
+        ));
+        $this->setData($results[0]);
+    }
+
+    /**
+     * Atualiza o cadastro de um usuário no banco de dados
+     */
+    public function update()
+    {
+        $sql = new Sql();
+        $results = $sql->select('CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)', array(
+            ':iduser' => $this->getiduser(),
+            ':desperson' => $this->getdesperson(),
+            ':deslogin' => $this->getdeslogin(),
+            ':despassword' => $this->getdespassword(),
+            ':desemail' => $this->getdesemail(),
+            ':nrphone' => $this->getnrphone(),
+            ':inadmin' => $this->getinadmin()
+        ));
+        $this->setData($results[0]);
+    }
+
+    /**
+     * Deleta um usuário no banco de dados
+     */
+    public function delete()
+    {
+        $sql = new Sql();
+        $sql->select('CALL sp_users_delete(:iduser)', array(
+            ':iduser' => $this->getiduser()
+        ));
+    }
+
 }
 
 ?>
